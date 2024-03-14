9c06f13331bec17ae95632f3d18fa38933ed3d7c
dude <ijfranco@gmail.com>
2024-03-14 01:17:42 -0500

    added summation of weeks.

--- src/DisplayAPIDataElement.js
+++ src/DisplayAPIDataElement.js
@@ -5,14 +5,17 @@ const DisplayAPIDataElement = ({ data }) => {
   const totalCommits = weeks.reduce((sum, week) => sum + week.c, 0);
   const totalLinesAdded = weeks.reduce((sum, week) => sum + week.a, 0);
   const totalLinesDeleted = weeks.reduce((sum, week) => sum + week.d, 0);
+  const totalWeeks = weeks.length;
 
 
   return (
     <div>
       <p>Author {author.login} has made: <br />
-      {totalCommits} commits<br />
-      {totalLinesAdded} lines added <br />
-      {totalLinesDeleted} lines deleted</p>
+        {totalCommits} commits<br />
+        {totalLinesAdded} lines added <br />
+        {totalLinesDeleted} lines deleted<br />
+        over {totalWeeks} weeks
+      </p>
       <ul>
         {weeks.map(week => (
             <li>week: {week.w} added: {week.a} deleted: {week.d} commits: {week.c}</li>
