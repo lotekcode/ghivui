85b072bace7c6361b15843c581c126fe4e9b1df2
Ian F <ijfranco@gmail.com>
2024-03-17 18:21:35 -0500

    updated date format in Commit Report

--- src/DisplayContributorsDataElement.js
+++ src/DisplayContributorsDataElement.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 const DisplayContributorDataElement = ({ data }) => {
   const { author, weeks } = data;
@@ -9,20 +10,40 @@ const DisplayContributorDataElement = ({ data }) => {
   const startingWeek = weeks[0].w;
   const endingWeek = weeks[weeks.length - 1].w;
 
-//test
+  const convertToWeeklyDate = (unixTimestamp) => {
+    const weeklyDate = new Date(unixTimestamp*1000);
+    return weeklyDate.toLocaleDateString();
+  }
+
   return (
     <div>
       <p>Author {author.login} has made: <br />
         {totalCommits} commits<br />
         {totalLinesAdded} lines added <br />
         {totalLinesDeleted} lines deleted<br />
-        over {totalWeeks} weeks from {startingWeek} through {endingWeek}
+        over {totalWeeks} weeks from {convertToWeeklyDate(startingWeek)} through {convertToWeeklyDate(endingWeek)}
       </p>
-      <ul>
-        {weeks.map(week => (
-            week.c !== 0 && <li key={week.w}>week: {week.w} added: {week.a} deleted: {week.d} commits: {week.c}</li>
-        ))}
-      </ul>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Week</th>
+            <th>Lines Added</th>
+            <th>Lines Deleted</th>
+            <th>Commits</th>
+          </tr>
+        </thead>
+        <tbody>
+          {weeks.map(week => (
+              week.c !== 0 && (
+              <tr key={week.w}>
+                <td>{convertToWeeklyDate(week.w)}</td>
+                <td>{week.a}</td> 
+                <td>{week.d}</td>
+                <td>{week.c}</td>
+              </tr> )
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
